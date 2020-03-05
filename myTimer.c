336c5759be393f241bb5991fef9c0243dfab8bb7
Youness Bella <ynbella@vt.edu>
2020-03-05 15:40:01 -0500

    Implemented myTimer.c and myTimer.h

--- myTimer.c
+++ myTimer.c
@@ -1,38 +1,44 @@
 /*
  * myTimer.c
  *
- *  Created on:
- *      Author:
+ *  Created on: 05 March 2020
+ *      Author: Youness Bella
  */
 
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 #include "myTimer.h"
 
-// TODO: Write a function that can intialize a Timer with particular values for pre-scale and count.
 // Aliases for the Timers and the preScaler arguments are defined in myTimer.h
 void initTimer(unsigned int timer, unsigned int preScaler, unsigned int count)
 {
     // For the specified timer and pre-scale value, put the timer in 32-bit periodic mode.
-
+    Timer32_initModule(timer, preScaler, TIMER32_32BIT, TIMER32_PERIODIC_MODE);
 
     // For the specified timer, pass the count value.
+    Timer32_setCount(timer, count);
 
 
     // For the specified timer, configure the timer to repeat once it elapses.
-
+    Timer32_startTimer(timer, REPEAT);
 }
 
-// TODO: Write a function that indicates whether Timer0 has expired.
 // You have been given a such a function in the lecture slides.
 bool timer0Expired(void)
 {
-
+    static uint32_t previousSnap = 0;
+    uint32_t currentSnap = Timer32_getValue(TIMER0);
+    bool returnValue = (currentSnap > previousSnap);
+    previousSnap = currentSnap;
+    return returnValue;
 }
 
-// TODO: Write a function that indicates whether Timer1 has expired.
 // This is essentially a copy of the previous function, using a different timer.
 // Since we only have two timers, later in the course we will create software timers that use a common time base.
 bool timer1Expired(void)
 {
-
+    static uint32_t previousSnap = 0;
+    uint32_t currentSnap = Timer32_getValue(TIMER1);
+    bool returnValue = (currentSnap > previousSnap);
+    previousSnap = currentSnap;
+    return returnValue;
 }
--- myTimer.h
+++ myTimer.h
@@ -1,8 +1,8 @@
 /*
  * myTimer.h
  *
- *  Created on:
- *      Author:
+ *  Created on: 05 March 2020
+ *      Author: Youness Bella
  */
 
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
@@ -25,11 +25,8 @@ bool timer1Expired(void);
 // This is an initial value for use in the TimerExpired functions.
 #define MAX_VALUE           0xFFFFFFFF
 
-// TODO: Determine values to represent the timer count values for one second (Timer0) and one millisecond (Timer1)
-#define TIMER0_COUNT        1
-#define TIMER1_COUNT        1
-
-// TODO: Define any constants that are local to myTimer.c using #define
+#define TIMER0_COUNT        3000000
+#define TIMER1_COUNT        3000
 
 
 #endif /* MYTIMER_H_ */
